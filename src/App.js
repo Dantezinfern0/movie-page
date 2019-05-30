6a199375ff32ec5aebf500b7e4a20b13f1cd2773
Allan Hall <bigal76126@gmail.com>
2019-05-30 16:37:20 -0400

    uh wut

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Homepage from './pages/Homepage'
 
 class App extends Component {
+  componentDidMount() {
+    
+  }
   render() {
     return <Homepage />
   }
