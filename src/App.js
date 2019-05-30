65251f9b22cbf96da2ece144349d34ba853066fb
Joe Baranski <joe.baranski49@gmail.com>
2019-05-30 14:45:16 -0400

    homepage basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import Homepage from './pages/Homepage'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <Homepage />
   }
 }
 
