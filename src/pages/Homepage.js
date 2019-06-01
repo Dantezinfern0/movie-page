4f65fbb073c98919c3d3ab28ee61d45d07771452
dantezinfern0 <danteharasz@gmail.com>
2019-06-01 12:54:24 -0400

    image not working yet

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,14 +7,13 @@ const apiKey = 'afc7a0ada171a2a77126fd0d421b758c'
 const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
 const apiUrlEnd = '&language=en-US&page=1'
 
+
 class Homepage extends Component {
   state = {
-    movieList: []
+    movieList: [],
+    images: []
   }
   componentDidMount() {
-    Axios.get(`${apiUrl}${apiKey}${apiUrlEnd}`).then(function(response) {
-      console.log(response)
-    })
     Axios.get(`${apiUrl}${apiKey}${apiUrlEnd}`).then(resp => {
       this.setState({
         movieList: resp.data.results
