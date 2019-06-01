bd7b38df5ce29ccd929da7a7cd609d3b14515a68
Joe Baranski <joe.baranski49@gmail.com>
2019-06-01 13:17:38 -0400

    got images to work

--- src/pages/IndividualMovies.jsx
+++ src/pages/IndividualMovies.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 // imagePreUrl includes the hard coded size of w500
 const imagePreUrl = 'https://image.tmdb.org/t/p/'
-const imageSize = 'w500/'
+const imageSize = 'w185/'
 
 class IndividualMovies extends Component {
   render() {
     return (
       <div>
         <h1 className="centered-text">{this.props.moOb.title}</h1>
-        {/* image is not working */}
-        <img src={`${imagePreUrl}${imageSize}${this.props.moOb.poster_url}`} />
+        <img src={`${imagePreUrl}${imageSize}${this.props.moOb.poster_path}`} />
+        {/* <img src="http://image.tmdb.org/t/p/w500/wgQ7APnFpf1TuviKHXeEe3KnsTV.jpg" /> working url path to test */}
         <h3 className="centered-text">{this.props.moOb.overview}</h3>
-        <h4 className="right-justify">Rating/Vote Score: {this.props.moOb.vote_average}</h4>
-        <h5 className="right-justify">Released: {this.props.moOb.release_date}</h5>
+        <h4 className="right-justify">
+          Rating/Vote Score: {this.props.moOb.vote_average}
+        </h4>
+        <h5 className="right-justify">
+          Released: {this.props.moOb.release_date}
+        </h5>
       </div>
     )
   }
