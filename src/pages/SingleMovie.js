9400afb86a252dcf3e20d4be2b65703a5fcc504e
dantezinfern0 <danteharasz@gmail.com>
2019-06-02 15:39:12 -0400

    comments

--- src/pages/SingleMovie.js
+++ src/pages/SingleMovie.js
@@ -3,8 +3,8 @@ import Axios from 'axios'
 import HomeButton from '../components/HomeButton.js'
 import IndividualMovies from '../components/IndividualMovies.jsx'
 
-const apiKey = 'afc7a0ada171a2a77126fd0d421b758c'
-const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
+const apiKey = '?api_key=afc7a0ada171a2a77126fd0d421b758c'
+const apiUrl = 'https://api.themoviedb.org/3/movie/'
 const apiUrlEnd = '&language=en-US&page=1'
 
 class SingleMovie extends Component {
@@ -12,11 +12,15 @@ class SingleMovie extends Component {
     movieInfo: []
   }
   componentDidMount() {
-    Axios.get(`${apiUrl}${apiKey}${apiUrlEnd}`).then(resp => {
+    // line below is supposed to match the movie ID
+    // to the api call so that the info is available here
+    // similar to how we did it for the cast page
+    //  the only difference is that we are not doing a .map
+    const movieId = this.props.match.params.id
+    Axios.get(`${apiUrl}${movieId}${apiKey}${apiUrlEnd}`).then(resp => {
       this.setState({
-        movieInfo: resp.data.results
+        movieInfo: resp
       })
-      console.log(this.state.movieInfo)
     })
   }
 
@@ -25,10 +29,10 @@ class SingleMovie extends Component {
       <div>
         <h1>This Is the Individual Movie Page</h1>
         <h2>
-          we need to figure out how fetch or set the state of movieInfo to align
-          with the movie Id number that the user clicked on on the home page
+          {this.state.movieInfo.original_title}
+          {/* <a href={this.state.movieInfo.homepage}>Movie Homepage</a> */}
         </h2>
-        <IndividualMovies moOb={this.state.movieInfo} />
+        {/* <IndividualMovies moOb={this.state.movieInfo} /> */}
         <HomeButton />
       </div>
     )
