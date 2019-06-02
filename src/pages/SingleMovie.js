9983f5142c604245a8892ed56f4628db89ae9b19
Joe Baranski <joe.baranski49@gmail.com>
2019-06-02 16:53:41 -0400

    got the api data to show on individual pages

--- src/pages/SingleMovie.js
+++ src/pages/SingleMovie.js
@@ -3,32 +3,42 @@ import Axios from 'axios'
 import HomeButton from '../components/HomeButton.js'
 import IndividualMovies from '../components/IndividualMovies.jsx'
 
-const apiKey = 'afc7a0ada171a2a77126fd0d421b758c'
-const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
-const apiUrlEnd = '&language=en-US&page=1'
+const imagePreUrl = 'https://image.tmdb.org/t/p/'
+const imageSize = ['w300', 'w780', 'w1280', 'original']
+const apiKey = '?api_key=afc7a0ada171a2a77126fd0d421b758c'
+// const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
+// const apiUrlEnd = '&language=en-US&page=1'
+const testUrl = 'https://api.themoviedb.org/3/movie/'
 
 class SingleMovie extends Component {
   state = {
-    movieInfo: []
+    movieTitle: '',
+    moviePoster: '',
+    movieTagLine: ''
   }
   componentDidMount() {
-    Axios.get(`${apiUrl}${apiKey}${apiUrlEnd}`).then(resp => {
+    const movieId = this.props.match.params.id
+    Axios.get(`${testUrl}${movieId}${apiKey}`).then(resp => {
+      console.log({ resp })
       this.setState({
-        movieInfo: resp.data.results
+        movieTitle: resp.data.original_title,
+        moviePoster: imagePreUrl + imageSize[0] + resp.data.backdrop_path,
+        movieTagLine: resp.data.tagline
       })
-      console.log(this.state.movieInfo)
+      console.log(this.state.moviePoster)
     })
   }
 
   render() {
     return (
       <div>
-        <h1>This Is the Individual Movie Page</h1>
-        <h2>
-          we need to figure out how fetch or set the state of movieInfo to align
-          with the movie Id number that the user clicked on on the home page
-        </h2>
-        <IndividualMovies moOb={this.state.movieInfo} />
+        <h1>{this.state.movieTitle}</h1>
+        <img src={`${this.state.moviePoster}`} />
+        <h2>{this.state.movieTagLine}</h2>
+        {/* <div>
+          {/* <IndividualMovies movie={this.state.movieInfo.original_title} /> */}
+        {/* </div>
+        <IndividualMovies moOb={this.state.movieInfo} /> */}{' '}
         <HomeButton />
       </div>
     )
