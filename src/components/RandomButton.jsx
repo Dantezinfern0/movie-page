6d309445bad4ce17ba706ac1051de4699d5193e7
Joe Baranski <joe.baranski49@gmail.com>
2019-06-02 14:13:48 -0400

    working on random button

---
+++ src/components/RandomButton.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class RandomButton extends Component {
+  render() {
+    return (
+      <div className="random-button-container">
+        <Link
+          classname="random-button-text"
+          // to={`/Movie/${
+          //   this.props.state.movieList[
+          //     Math.floor(Math.random() * this.props.state.movieList.length)
+          //   ]
+          // }`}
+          to="/Movie/420817"
+        >
+          To random movie...
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default RandomButton
