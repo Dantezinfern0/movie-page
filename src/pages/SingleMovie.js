67fdc190c2e026744cdb54eb0e126e4c629f13fb
Joe Baranski <joe.baranski49@gmail.com>
2019-06-02 16:57:44 -0400

    woops fixed

--- src/pages/SingleMovie.js
+++ src/pages/SingleMovie.js
@@ -3,18 +3,12 @@ import Axios from 'axios'
 import HomeButton from '../components/HomeButton.js'
 import IndividualMovies from '../components/IndividualMovies.jsx'
 
-<<<<<<< HEAD
 const imagePreUrl = 'https://image.tmdb.org/t/p/'
 const imageSize = ['w300', 'w780', 'w1280', 'original']
 const apiKey = '?api_key=afc7a0ada171a2a77126fd0d421b758c'
 // const apiUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
 // const apiUrlEnd = '&language=en-US&page=1'
 const testUrl = 'https://api.themoviedb.org/3/movie/'
-=======
-const apiKey = '?api_key=afc7a0ada171a2a77126fd0d421b758c'
-const apiUrl = 'https://api.themoviedb.org/3/movie/'
-const apiUrlEnd = '&language=en-US&page=1'
->>>>>>> 1e0a7c6dae6cc6cc95d71b4bb6e6a823ae15350b
 
 class SingleMovie extends Component {
   state = {
