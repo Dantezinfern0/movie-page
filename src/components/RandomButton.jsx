a0fdeb30a494d3f5c635a4b38347c1e0aae9c8c4
Allan Hall <bigal76126@gmail.com>
2019-06-02 21:53:34 -0400

    got random movie button to work

--- src/components/RandomButton.jsx
+++ src/components/RandomButton.jsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+const id = [
+  420817,
+  458156,
+  299534,
+  373571,
+  447404,
+  535581,
+  531309,
+  438650,
+  412117,
+  505600,
+  449562,
+  504608,
+  502416,
+  480414,
+  456740,
+  537915,
+  157433,
+  529983,
+  527641,
+  455957
+]
+
 class RandomButton extends Component {
   render() {
     return (
       <div className="random-button-container">
         <Link
           className="random-button-text"
-          // to={`/Movie/${
-          //   this.props.state.movieList[
-          //     Math.floor(Math.random() * this.props.state.movieList.length)
-          //   ]
-          // }`}
-          to="/Movie/420817"
+          to={`/Movie/${id[Math.floor(Math.random() * 20)]}`}
         >
           To random movie...
         </Link>
